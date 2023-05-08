693cf3cf6e796b2b583817ec5171c69ee327aa4e
설찬우 <60384142+Seol0716@users.noreply.github.com>
2023-05-08 23:12:22 +0900

    Delete Todo_project directory

--- Todo_project/app/src/main/java/com/example/todo_project/Data.kt
+++
@@ -1,9 +0,0 @@
-package com.example.todo_project
-
-data class Data(
-    var id: Int,
-    var complete: Boolean,
-    val title: String,
-    var delte_boolean: Boolean,
-
-)
--- Todo_project/app/src/main/java/com/example/todo_project/Month_data.kt
+++
@@ -1,9 +0,0 @@
-package com.example.todo_project
-
-data class Month_data(
-    var year: Int,
-    var month: Int,
-)
-
-
-
