3059d2475745692f31bc58adeecde0db7594d231
설찬우 <cksdn1092@naver.com>
2023-05-09 17:46:19 +0900

    kotlin-andorid-extensions 사용

--- jetpack/app/src/main/java/com/example/jectpack_library/MainActivity.kt
+++ jetpack/app/src/main/java/com/example/jectpack_library/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.jectpack_library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+//findVeiwById
+//kotlin Extensions
+//뷰의 null의 이슈
+//성능이슈
+//ViewBinding
+//Data Binding
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        //findViewById -> 100개
+        val btn1: Button = findViewById(R.id.btn1)
+        val btn2: Button = findViewById(R.id.btn2)
+        val btn3: Button = findViewById(R.id.btn3)
+
+        btn1.text = "abcd"
+        btn2.text = "abcd"
+        btn3.text = "abcd"
+        */
+
+
     }
 }
