d3b001379eb280583a616024ccb680bbf08eb883
설찬우 <cksdn1092@naver.com>
2023-05-17 16:31:58 +0900

    데이터 바이딩과 클래스 연습

--- DataBinding_DataClass/app/src/main/java/com/example/databinding_dataclass/MainActivity.kt
+++ DataBinding_DataClass/app/src/main/java/com/example/databinding_dataclass/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.databinding_dataclass
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.databinding_dataclass.databinding.ActivityMainBinding
+
+//DataBinding
+//이름처럼 뭔가 데이터를 어쩌고 저쩌고 해줄 수 없을까??? 연결
+//데이터 결합
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    var testCount = 20
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //주의 정수형은 문자형으로 형변환환
+       val person  = Person("개복치",20)
+        binding.user = person
+    }
+
+    fun myClick(view : View){
+        Log.d("MainActivity","on")
+        testCount++
+
+        val person  = Person("개복치",testCount)
+        binding.user = person
     }
 }
---
+++ DataBinding_DataClass/app/src/main/java/com/example/databinding_dataclass/Person.kt
@@ -0,0 +1,6 @@
+package com.example.databinding_dataclass
+
+class Person(
+    val name: String,
+    val age : Int
+)
