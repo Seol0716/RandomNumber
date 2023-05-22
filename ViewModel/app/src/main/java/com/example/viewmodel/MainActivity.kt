527a62b35ad88f0beb1622f1b0fa25782152c84f
설찬우 <cksdn1092@naver.com>
2023-05-22 16:08:12 +0900

    ViewModel 개념정리 & 생명주기 확인

---
+++ ViewModel/app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+
+//안드로이드 샘영 주기를 관리를 관리하기 쉽다.
+//onSaveInstanceState()를 사용해서 데이터를 관리할 수 있지만 소량의 데이터를 위주로 사용하는게 쉽다.
+//ViewModel은 대량의 데이터를 사용하기에 적합하고 상태 변환 있을때마다 관리하기 편하다.
+//Ui컨트롤러로 ViewModel을 사용하면 테스나 관리에 용이하다
+//ViewModel은 ViewModel scope에서 생명주기 전체를 관리한다.
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity","onCreate")
+
+        val plusBtn : Button = findViewById<Button>(R.id.plus)
+        val minusBtn : Button = findViewById<Button>(R.id.minus)
+
+        val result_text : TextView = findViewById(R.id.result)
+
+        plusBtn.setOnClickListener{
+            count++
+
+            result_text.text = count.toString()
+        }
+
+        minusBtn.setOnClickListener {
+            count--
+
+            result_text.text = count.toString()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("MainActivity","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("MainActivity","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("MainActivity","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("MainActivity","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("MainActivity","onDestroy")
+    }
+
+}
