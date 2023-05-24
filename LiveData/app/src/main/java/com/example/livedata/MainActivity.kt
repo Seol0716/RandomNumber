24cf22f984a9811245138d4e5900dd48c191cfed
설찬우 <cksdn1092@naver.com>
2023-05-24 22:29:23 +0900

    DiveData와 fragment의 연결 + lifcyclerOwner

--- LiveData/app/src/main/java/com/example/livedata/MainActivity.kt
+++ LiveData/app/src/main/java/com/example/livedata/MainActivity.kt
@@ -8,32 +8,68 @@ import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.livedata.databinding.ActivityMainBinding
+import com.example.livedata.databinding.ActivitytestBinding
 
 //Live Data의 개념
 //LiveData와 ViewModel과 결합
-class MainActivity : AppCompatActivity() {
+//LiveData와 MulableLiveData의 차이점
+//fragment LiveData / viewlifecyclerOwner
 
-    private lateinit var mainViewModel: MainViewModel
-    private var testMutableLiveData  = MutableLiveData(0)
+
+class MainActivity : AppCompatActivity() {
+//
+//    private lateinit var mainViewModel: MainViewModel
+//    private var testMutableLiveData  = MutableLiveData(0)
+
+    private lateinit var binding  : ActivitytestBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding = ActivitytestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<Button>(R.id.btn).setOnClickListener{
-            // testMutableLiveData.value = testMutableLiveData.value!!.plus(1)
-            mainViewModel.plusLiveDataValue()
+        val manager = supportFragmentManager
+
+        binding.btn1.setOnClickListener {
+            val transaction = manager.beginTransaction()
+            val fragment1 = BlankFragment1()
+
+            transaction.replace(R.id.frame,fragment1).addToBackStack(null).commit()
         }
 
-        //데이터를 관찰
-        mainViewModel.testMutableLiveData.observe(this, Observer {
-            //Log.d("MainActivity",testMutableLiveData.value.toString())
-            //findViewById<TextView>(R.id.textValue).text = testMutableLiveData.value.toString()
+        binding.btn2.setOnClickListener {
+            val transaction = manager.beginTransaction()
+            val fragment2 = BlankFragment2()
 
-            //viewModel의 MulableLiveData를 가져와서 저장
-            findViewById<TextView>(R.id.textValue).text = mainViewModel.testMutableLiveData.value.toString()
-        })
+            transaction.replace(R.id.frame,fragment2).addToBackStack(null).commit()
+        }
+
+
+//        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+//
+//        findViewById<Button>(R.id.btn).setOnClickListener{
+//          mainViewModel.plusLiveDataValue()
+//        }
+//
+//        mainViewModel.testLiveData.observe(this, Observer {
+//            findViewById<TextView>(R.id.textValue).text = it.toString()
+//        })
+
+//        mainViewModel.testLiveData.value = 10
+//
+//        데이터를 관찰
+//        mainViewModel.testMutableLiveData.observe(this, Observer {
+//            //Log.d("MainActivity",testMutableLiveData.value.toString())
+//            //findViewById<TextView>(R.id.textValue).text = testMutableLiveData.value.toString()
+
+//            //viewModel의 MulableLiveData를 가져와서 저장
+//            findViewById<TextView>(R.id.textValue).text = it.toString()
+//        })
+//
+//        mainViewModel.testMutableLiveData.value = 10
+
+
     }
 }
