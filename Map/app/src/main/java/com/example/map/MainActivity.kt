6abd20975806e3091e46e3f7bed2fadb96e88cad
설찬우 <cksdn1092@naver.com>
2023-05-25 22:14:48 +0900

    카운트앱2

--- Map/app/src/main/java/com/example/map/MainActivity.kt
+++ Map/app/src/main/java/com/example/map/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+
         //버튼 클릭시 카운트의 값을 뷰 모델의 LiveData에 저장
         binding.btn.setOnClickListener {
             val count = binding.edit.text.toString().toInt()
@@ -24,11 +25,31 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        //관찰
-        viewModel.liveCount.observe(this, Observer {
-            binding.text1.text = (it + it).toString()
-            binding.text2.text = (it * it).toString()
+        //map
+        viewModel.plus_data.observe(this, Observer {
+            binding.text1.text = it.toString()
+
         })
 
+        viewModel.gob_data.observe(this, Observer {
+            binding.text2.text = it.toString()
+        })
+
+        //switch
+        //+
+        viewModel.plus_LiveData.observe(this, Observer {
+            binding.text1.text = it.toString()
+        })
+
+        viewModel.gob_LiveData.observe(this, Observer {
+            binding.text2.text = it.toString()
+        })
+//
+//        //관찰
+//        viewModel.liveCount.observe(this, Observer {
+////            binding.text1.text = (it + it).toString()
+////              binding.text2.text = (it * it).toString()
+//
+//        })
     }
 }
--- Map/app/src/main/java/com/example/map/MainViewModel.kt
+++ Map/app/src/main/java/com/example/map/MainViewModel.kt
@@ -2,11 +2,16 @@ package com.example.map
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    //Transformations
+    //Transformations -> Map / switchMap
+
+    //map -> 값을 return
+
+
     private var valueCount = MutableLiveData(0)
     val liveCount : LiveData<Int>
     get() = valueCount
@@ -17,4 +22,37 @@ class MainViewModel : ViewModel() {
         valueCount.value = count
     }
 
+
+    //Map
+    //map은 LiveData의 값을 반환
+    //더하기
+    val plus_data = Transformations.map(liveCount){
+        it + it
+    }
+
+    //곱하기
+    val gob_data = Transformations.map(liveCount){
+        it * it
+    }
+
+    //Switch
+    val gob_LiveData = Transformations.switchMap(liveCount){
+        plus_Value(it)
+    }
+
+    //더하기
+    fun plus_Value(count : Int) : MutableLiveData<Int> {
+        val testData  = MutableLiveData(count + count)
+        return testData
+    }
+    //switch는 LiveData를 반환
+    val plus_LiveData = Transformations.switchMap(liveCount){
+        gob_Value(it)
+    }
+
+    //switch 곱하기
+    fun gob_Value(count : Int) : MutableLiveData<Int> {
+        val testData  = MutableLiveData(count * count)
+        return testData
+    }
 }
