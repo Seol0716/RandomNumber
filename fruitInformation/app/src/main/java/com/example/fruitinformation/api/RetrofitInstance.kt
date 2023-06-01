a16898256f33dd07a4e1d42a09f20bca0d33b940
설찬우 <cksdn1092@naver.com>
2023-06-01 21:45:14 +0900

    fruit

---
+++ fruitInformation/app/src/main/java/com/example/fruitinformation/api/RetrofitInstance.kt
@@ -0,0 +1,19 @@
+package com.example.fruitinformation.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+
+    val Base_URI = "https://raw.githubusercontent.com/"
+
+    val client = Retrofit
+        .Builder().baseUrl(Base_URI)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getInstance() : Retrofit {
+        return client
+    }
+
+}
