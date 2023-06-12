85e1a556413053c8d8566df7d048d1163fddffa4
설찬우 <cksdn1092@naver.com>
2023-06-12 16:10:45 +0900

    Repository 사용

---
+++ RetrofitExample3/app/src/main/java/com/example/retrofitexample3/CustomAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.retrofitexample3
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.retrofitexample3.Model.Plant
+
+class CustomAdapter(val context : Context, val dataSet : List<Plant>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        val textView : TextView = view.findViewById(R.id.textArea)
+        val imageView : ImageView = view.findViewById(R.id.imageArea)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.text_row_item, parent, false)
+
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
+
+        holder.textView.text = dataSet[position].name
+        Glide.with(context)
+            .load(dataSet[position].imageUrl)
+            .into(holder.imageView)
+
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+}
