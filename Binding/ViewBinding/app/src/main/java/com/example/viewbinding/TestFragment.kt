2da865697a8f2499f78bf34d2d6e13bd0a824e1b
설찬우 <cksdn1092@naver.com>
2023-06-14 23:59:10 +0900

    ViewBinding

---
+++ Binding/ViewBinding/app/src/main/java/com/example/viewbinding/TestFragment.kt
@@ -0,0 +1,34 @@
+package com.example.viewbinding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewbinding.databinding.FragmentTestBinding
+
+class TestFragment : Fragment() {
+
+    private var _binding : FragmentTestBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        _binding = FragmentTestBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.fragmentText.text= "이거는 framgent Text"
+
+        return view
+    }
+
+
+}
