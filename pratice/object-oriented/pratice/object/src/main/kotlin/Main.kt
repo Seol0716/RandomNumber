4a9bb75514d8c6ed254a6c29ce2d4536678c205e
snow <cksdn1092@naver.com>
2023-12-17 17:13:31 +0900

    객체지향, 생성자 정리

--- pratice/object-oriented/pratice/object/src/main/kotlin/Main.kt
+++ pratice/object-oriented/pratice/object/src/main/kotlin/Main.kt
@@ -4,20 +4,59 @@ fun main() {
     //객체지향 프로그래밍
     //생성자
 
-    //객체생성
-    val testclass = TestClass()
-    val testclass2 = TestClass(1,2)
-    //멤버변수
+//
+//    println("testvar : ${testclass.testvar}")
+//    //멤버함수
+//    testclass.testmethod()
+//    println("${testclass2.v1} ${testclass2.v2}")
 
-    println("testvar : ${testclass.testvar}")
-    //멤버함수
-    testclass.testmethod()
-    println("${testclass2.v1} ${testclass2.v2}")
 
+    var tst3 = TstClass3("hee", 26);
+    var tst4 = TstClass3("ri", 1, "010-1234-1234");
+
+    println("tst3.name : ${tst3.name}");
+    println("tst3.age : ${tst3.age}");
+
+    println("tst4.name : ${tst4.name}");
+    println("tst4.age : ${tst4.age}");
+
+    val p = Person("홍길동")
+    var person = Person("김루피",11,"010-1111-2222")
+}
+
+
+class Person(name : String) {
+    var name : String
+    var age : Int
+    var phoneNumber : String
+
+    init {
+        println("init 실행")
+        this.name = name
+        this.age = 0
+        this.phoneNumber = " "
+        println("${name} ${age}")
+    }
+
+    constructor(name : String, age : Int , phoneNumber: String) : this(name){
+        println("보조 생성자 실행")
+        println("${name} ${age} ${phoneNumber}")
+
+    }
 
 }
 
-class TestClass {
+class person(val name : String, val age : Int, val phonenumber : String){
+
+    init {
+        this.name
+        this.age
+        this.phonenumber
+    }
+
+
+}
+class x {
 
     var v1 : Int = 0
     var v2 : Int = 0
@@ -46,7 +85,16 @@ class TestClass {
     constructor(i : Int , j : Int ) {
         v1 = i
         v2 = j
+}
+}
+
+
+class TstClass3 constructor(name: String, age : Int) {
+    var name =name;
+    var age = age;
+    var phoneNumber = "";
+
+    constructor(name: String, age : Int, phoneNumber : String) : this(name, age) {
+        this.phoneNumber = phoneNumber;
     }
-
-
 }
