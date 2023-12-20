137be60b646b8d40dbe701d5a8061e8fd4b1fe14
설찬우 <60384142+Seol0716@users.noreply.github.com>
2023-12-20 18:40:25 +0900

    Update DB.kt

--- Project/app/src/main/java/com/example/project/DB/DB.kt
+++ Project/app/src/main/java/com/example/project/DB/DB.kt
@@ -9,6 +9,13 @@ import com.example.project.DB.DataEntity.Data
 
 
 //Room db와 연결
+//데이터 베이스 구성을 정의하고 데이터 액세스 접근하기 위한 역할을 한다
+//조건 
+//1. @Database 키워드를 선언하고 entity 클래스를 정의해야한다
+//2. RoomDatabase 추상 클래스 선언
+//3. Dao 클래스에서 DB 클래스 인수가 0개여야 하고
+// Dao 클래스의 인스턴스를 반환하는 추상 메서드를 정의해야한다
+
 @Database(entities = [Data::class], version = 1)
 
 abstract class DB : RoomDatabase() {
