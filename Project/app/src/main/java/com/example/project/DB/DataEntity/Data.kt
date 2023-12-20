792ca853711aca41f0da8e3f6039f7ec1065730a
설찬우 <60384142+Seol0716@users.noreply.github.com>
2023-12-20 18:52:49 +0900

    Data entity 개념 정리

--- Project/app/src/main/java/com/example/project/DB/DataEntity/Data.kt
+++ Project/app/src/main/java/com/example/project/DB/DataEntity/Data.kt
@@ -4,13 +4,12 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
+//데이터 항목 정의
+//테이블 이름, 기본키, 필드 name, id를 정의
 @Entity(tableName = "name_table")
 data class Data(
 
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    var id : Int,
+    @PrimaryKey val id : Int,
     @ColumnInfo(name = "data")
-    var name : String
+    var data : String
 )
