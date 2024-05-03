729d2ce0eb19b9703ede340b8017b869184b4cac
snow <cksdn1092@naver.com>
2024-05-03 23:00:11 +0900

    View, 클래스 파일 수정

---
+++ mvvm_pratice/app/src/main/java/com/example/mvvm_pratice/Model/DataEntity.kt
@@ -0,0 +1,9 @@
+package com.example.mvvm_pratice.Model
+
+data class DataEntity(
+
+    //고유번호
+    //당첨자 번호
+    var id : Int = 0,
+    val number : Int = 0
+)
--- mvvm_pratice/app/src/main/java/com/example/mvvm_pratice/ViewModel/MainViewModel.kt
+++ mvvm_pratice/app/src/main/java/com/example/mvvm_pratice/ViewModel/MainViewModel.kt
@@ -7,55 +7,29 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.mvvm_pratice.Model.Repository
-import com.example.mvvm_pratice.Room.DataEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
-class MainViewModel(private val repository: Repository) : ViewModel() {
+class MainViewModel :ViewModel() {
 
+    //뽑힌 번호
+    private var _winner = MutableLiveData<Int>()
 
-    //카운트 데이터 셋
-    private var _countData = MutableLiveData<Int>(0)
-
-    val countData : LiveData<Int>
-        get() = _countData
-
-   private val _name = MutableLiveData<String>()
-    val name : LiveData<String>
-        get() = _name
-
-
-    //마이너스
-    fun minus_count() {
-        _countData.value = _countData.value!!.minus(1)
+    val winner : LiveData<Int>
+        get() = _winner
+    
+    init {
+        _winner.value = 0
     }
 
-    //카운트 플러스
-    fun plus_count() {
-        _countData.value = _countData.value!!.plus(1)
+    //랜덤번호 뽑기
+
+    fun getRandomNumber( number : Int){
+
+        val random_number = Random.nextInt(number)
+        _winner.value = random_number
     }
-
-    fun reset_count() {
-        _countData.value = 0
-    }
-
-    fun getRandomName() {
-
-        viewModelScope.launch {
-            val randomId = repository.getRandomId()
-            val name = repository.getNameById(randomId)
-            _name.postValue(name.toString())
-        }
-    }
-
-    fun insert(name : String) = CoroutineScope(Dispatchers.IO).launch {
-        repository.insert(name)
-    }
-
-//    fun Delete() = CoroutineScope(Dispatchers.IO).launch {
-//        repository.getDelete()
-//    }
 }
